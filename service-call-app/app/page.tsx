40ef059fea6f70d60e339f9992012105919309fa
Eric-Hinderer <ericjh@iorb.com>
2024-08-20 10:29:34 -0500

    Dashboard page

--- service-call-app/app/page.tsx
+++ service-call-app/app/page.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
 export default async function Home() {
-const data = await fetch('https://catfact.ninja/fact').then((res) => res.json());
-
   return (
-    <div>
-      <Link href="/dashboard">DashBoard </Link>
-      <p>{data.fact}</p>
-    </div>
+    <main>
+      <div>
+        <Link href="/dashboard">DashBoard </Link>
+      </div>
+    </main>
   );
 }
