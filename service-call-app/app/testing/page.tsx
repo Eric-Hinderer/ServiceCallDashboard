ee01bb9c69fe5a64cc65b1e2e1233d663d282406
ehinderer469 <ericjh@iorb.com>
2024-08-21 13:48:56 -0500

    real time update wip

---
+++ service-call-app/app/testing/page.tsx
@@ -0,0 +1,10 @@
+import RealTimeData from "../RealTimeData";
+
+export default function testPage() {
+  return (
+    <div className="pt-20">
+      <h1>Test Page</h1>
+      <RealTimeData />
+    </div>
+  );
+}
