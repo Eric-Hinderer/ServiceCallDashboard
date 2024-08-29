2acc6f3dfd3de561b1d89f8d21f2c856e3d0eeaf
ehinderer469 <ericjh@iorb.com>
2024-08-29 09:54:20 -0500

    analytics

--- service-call-app/app/analytics/action.ts
+++ service-call-app/app/analytics/action.ts
@@ -13,7 +13,6 @@ import { DateTime } from "luxon";
 
 import { ServiceCall } from "../(definitions)/definitions";
 
-
 interface CallsByDay {
   callCount: number;
   serviceCalls: any[];
@@ -22,8 +21,12 @@ interface CallsByDay {
 export async function getWeekendServiceCalls(startDate: Date, endDate: Date) {
   const serviceCallsRef = collection(db, "ServiceCalls");
 
-  const startInCentralTime = DateTime.fromJSDate(startDate, { zone: 'UTC' }).setZone('America/Chicago');
-  const endInCentralTime = DateTime.fromJSDate(endDate, { zone: 'UTC' }).setZone('America/Chicago');
+  const startInCentralTime = DateTime.fromJSDate(startDate, {
+    zone: "UTC",
+  }).setZone("America/Chicago");
+  const endInCentralTime = DateTime.fromJSDate(endDate, {
+    zone: "UTC",
+  }).setZone("America/Chicago");
 
   const q = query(
     serviceCallsRef,
@@ -59,9 +62,12 @@ export async function getAfterHoursCallsByDayOfWeek(
 ): Promise<{ dayOfWeek: number; callCount: number; serviceCalls: any[] }[]> {
   const serviceCallsRef = collection(db, "ServiceCalls");
 
-  const startInCentralTime = DateTime.fromJSDate(startDate, { zone: 'UTC' }).setZone('America/Chicago');
-const endInCentralTime = DateTime.fromJSDate(endDate, { zone: 'UTC' }).setZone('America/Chicago');
-
+  const startInCentralTime = DateTime.fromJSDate(startDate, {
+    zone: "UTC",
+  }).setZone("America/Chicago");
+  const endInCentralTime = DateTime.fromJSDate(endDate, {
+    zone: "UTC",
+  }).setZone("America/Chicago");
 
   const q = query(
     serviceCallsRef,
@@ -70,22 +76,29 @@ const endInCentralTime = DateTime.fromJSDate(endDate, { zone: 'UTC' }).setZone('
   );
 
   const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    const date: Date = doc.data().date.toDate();
+    console.log(date.getTimezoneOffset());
+  });
 
- 
   const callsByDayOfWeek: { [key: string]: CallsByDay } = {};
 
   querySnapshot.forEach((doc) => {
     const data = doc.data();
 
-  
-   const localDate = data.date.toDate();
+    const localDate = data.date.toDate();
+    console.log("local date" + localDate.getTimezoneOffset());
 
-    const hourOfDay = localDate.getHours();
-    const dayOfWeek = localDate.getDay(); 
+    let hourOfDay = localDate.getUTCHours() - 5;
+    const dayOfWeek = localDate.getDay();
 
+    if (hourOfDay < 0) {
+      hourOfDay += 24;
+    }
 
     if (
-      (hourOfDay >= 17 && hourOfDay <= 24 || hourOfDay >= 0  && hourOfDay <= 8) &&
+      ((hourOfDay >= 17 && hourOfDay < 24) ||
+        (hourOfDay >= 0 && hourOfDay <= 8)) &&
       dayOfWeek >= 1 &&
       dayOfWeek <= 5
     ) {
