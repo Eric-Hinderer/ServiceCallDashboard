676e0fe865e7e3ad72342265fddf7b7cf2e62333
ehinderer469 <ericjh@iorb.com>
2024-09-18 18:18:56 -0500

    test

--- service-call-app/app/layout.tsx
+++ service-call-app/app/layout.tsx
@@ -1,12 +1,43 @@
 import Header from "@/components/NavBar";
 import "./globals.css";
+import * as React from "react";
 import { AuthProvider } from "@/components/AuthContext";
+import { AppProvider } from "@toolpad/core/nextjs";
+import { Home } from "@mui/icons-material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+
+import BarChartIcon from "@mui/icons-material/BarChart";
+
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import type { Router, Navigation } from "@toolpad/core";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const NAVIGATION: Navigation = [
+    {
+      kind: "header",
+      title: "Main items",
+    },
+    {
+      segment: "",
+      title: "Home",
+      icon: <Home />,
+    },
+    {
+      segment: "dashboard",
+      title: "Dashboard",
+      icon: <DashboardIcon />,
+    },
+    {
+      segment: "analytics",
+      title: "Analytics",
+      icon: <BarChartIcon />,
+    },
+  ];
+
   return (
     <html lang="en">
       <head>
@@ -20,8 +51,15 @@ export default function RootLayout({
 
       <body>
         <AuthProvider>
-          <Header />
-          <body>{children}</body>
+          <AppProvider
+            navigation={NAVIGATION}
+            branding={{
+              logo: <img src="/central.jpg" alt="logo" />,
+              title: "Central Distributing",
+            }}
+          >
+            <DashboardLayout>{children}</DashboardLayout>
+          </AppProvider>
         </AuthProvider>
       </body>
     </html>
