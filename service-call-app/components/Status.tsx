1df490638a8fd51df7e23372457896962a8cd274
ehinderer469 <ericjh@iorb.com>
2024-09-18 15:35:13 -0500

    new status Icons

--- service-call-app/components/Status.tsx
+++ service-call-app/components/Status.tsx
@@ -8,7 +8,21 @@ import {
   SelectContent,
   SelectItem,
   SelectValue,
+  SelectGroup,
 } from "@/components/ui/select";
+import { Circle, Clock, CheckCircle } from "lucide-react";
+import React from "react";
+
+const statusOptions = [
+  { value: "OPEN", label: "Open", color: "text-blue-500", icon: Circle },
+  {
+    value: "IN_PROGRESS",
+    label: "In Progress",
+    color: "text-yellow-500",
+    icon: Clock,
+  },
+  { value: "DONE", label: "Done", color: "text-green-500", icon: CheckCircle },
+];
 
 export default function Status({
   id,
@@ -32,21 +46,38 @@ export default function Status({
     });
   };
 
+  const selectedOption = statusOptions.find(
+    (option) => option.value === status
+  );
+
   return (
-    <Select
-    value={status}
-    onValueChange={handleStatusChange}
-    disabled={isPending}
-  >
-    <SelectTrigger className="text-left min-w-[121px] py-1 px-2 h-8">
-      <SelectValue placeholder="Select status..." />
-    </SelectTrigger>
-    <SelectContent>
-      <SelectItem value="OPEN">Open</SelectItem>
-      <SelectItem value="IN_PROGRESS">In Progress</SelectItem>
-      <SelectItem value="DONE">Done</SelectItem>
-    </SelectContent>
-  </Select>
-  
+    <Select onValueChange={handleStatusChange} value={status}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select status">
+          {selectedOption && (
+            <div className={`flex items-center ${selectedOption.color}`}>
+              {React.createElement(selectedOption.icon, {
+                className: "mr-2 h-4 w-4",
+              })}
+              {selectedOption.label}
+            </div>
+          )}
+        </SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {statusOptions.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              <div className={`flex items-center ${option.color}`}>
+                {React.createElement(option.icon, {
+                  className: "mr-2 h-4 w-4",
+                })}
+                {option.label}
+              </div>
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 }
