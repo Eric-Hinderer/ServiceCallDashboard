725f3498d52ffbc9a5b39412b12a80b1117c309b
ehinderer469 <ericjh@iorb.com>
2024-09-23 11:06:21 -0500

    got rid of random details

--- service-call-app/app/page.tsx
+++ service-call-app/app/page.tsx
@@ -7,42 +7,30 @@ export default function Home() {
   return (
     <Container
       style={{
-        padding: "2rem",
+        padding: "0.5rem",
         backgroundColor: "#f4f6f9",
         width: "100%",
         maxWidth: "2000px",
       }}
     >
-      {/* Dashboard Header */}
-      <Box textAlign="center" marginBottom="2rem">
-        <Typography
-          variant="h4"
-          component="h1"
-          gutterBottom
-          style={{ fontWeight: "600", color: "#333" }}
-        >
-          Service Call Dashboard
-        </Typography>
-        <Typography variant="subtitle1" color="textSecondary">
-          Overview of ongoing and completed service calls
-        </Typography>
-      </Box>
-
       {/* Current Service Calls */}
-      <Box marginBottom="2rem">
-        <Typography
-          variant="h5"
-          gutterBottom
-          style={{ fontWeight: "500", textAlign: "center", color: "#37474f" }}
-        >
-          Current Service Calls (Open/In Progress)
-        </Typography>
-      </Box>
 
       {/* Service Call List (RealTime Component) */}
       <Grid size={{ xs: 12 }}>
         <Card>
           <CardContent>
+            <Box>
+              <Typography
+                variant="h5"
+                style={{
+                  fontWeight: "500",
+                  textAlign: "center",
+                  color: "#37474f",
+                }}
+              >
+                Current Service Calls (Open/In Progress)
+              </Typography>
+            </Box>
             <RealTimeOpenInProgress />
           </CardContent>
         </Card>
